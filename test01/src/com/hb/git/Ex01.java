80a3f01d33bb3a3b818e5a5208ff838aad9ad761
miran <k1025mr@gmail.com>
2017-11-14 17:53:18 +0900

    commit

---
+++ test01/src/com/hb/git/Ex01.java
@@ -0,0 +1,20 @@
+package com.hb.git;
+
+import java.util.Date;
+
+public class Ex01 {
+	
+	public int sabun;
+	protected String name;
+	Date nalja;
+	private int pay;;
+	
+	public void func01(){}
+	
+	private int func02(){
+		return 0;
+	}
+	protected void func03(){
+		
+	}
+}
